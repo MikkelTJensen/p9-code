230cf98a213e9c9759a8ac6e5b2691ddc4654fbc
Mikkel Jensen <mikkeljensen@h41.aau1x.klient.slv.site.aau.dk>
2022-09-29 16:32:22 +0200

    made class for socket connection

--- proxy-server.py
+++ proxy-server.py
@@ -6,20 +6,40 @@ HOST = "127.0.0.1"
 PORT = 65432
 
 
-def on_new_client(conn, addr):
-	with conn:
-		status = None
-		while True:
-			data = conn.recv(1024)
-			if data:
-				conn.sendall(data)
-				break
+class Server():
+	def __init__():
+		pass
 
+
+class SocketConnection():
+	def __init__(self, conn, addr):
+		self.conn = conn
+		self.addr = addr
+		self.conn_type = None
+
+	def run(self):
+		with self.conn:
+			while True:
+				data = self.conn.recv(1024)
+				if data and not self.handleMessage(data):
+					break
+
+	def handleMessage(self, data):
+		msg = f"{data!r}"
+		print(msg)
+		self.conn.sendall(data)
+		return False
+
+
+def on_new_client(conn, addr):
+	socket_connection = SocketConnection(conn, addr)
+	socket_connection.run()
+	
 
 def main():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.bind((HOST, PORT))
-		s.listen(10)
+		s.listen(2)
 		while True:
 			conn, addr = s.accept()
 			new_thread = threading.Thread(target=on_new_client, args=(conn, addr))
