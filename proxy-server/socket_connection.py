b06905d497a4aac2889ac3f9ea7db9e94415c831
Mikkel Jensen <mikkeljensen@h262.aau1x.klient.slv.site.aau.dk>
2022-09-30 11:34:56 +0200

    messaging between 2 clients works a little better now

--- proxy-server/socket_connection.py
+++ proxy-server/socket_connection.py
@@ -8,15 +8,24 @@ class SocketConnection():
 	def run(self):
 		with self.conn:
 			while True:
-				# Incoming from sender/receiver
-				data = self.conn.recv(1024)
-				if data:
-					self.handle_message(data)
-				# Send message back to sender/receiver (if any in buffer)
-				msg = self.message_buffer.check_for_message(self.socket_id)
-				if msg:
-					self.conn.sendall(msg)
+				inc = self.handle_incoming()
+				out = self.handle_outgoing()
+				if inc or out:
+					break	
 
-	def handle_message(self, data):
-		msg = data.decode()
-		self.message_buffer.store_message(msg, self.socket_id)
+	def handle_incoming(self):
+		try:
+			self.conn.settimeout(2.0)
+			data = self.conn.recv(1024)
+			if data:
+				msg = data.decode()
+				self.message_buffer.store_message(msg, self.socket_id)
+		except:
+			pass
+		return False
+
+	def handle_outgoing(self):
+		msg = self.message_buffer.check_for_message(self.socket_id)
+		if msg:
+			self.conn.sendall(msg)
+		return False
