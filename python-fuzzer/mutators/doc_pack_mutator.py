a343b42ff0f205c423bf2e01c6b11e5621246da8
Mikkel Jensen <mikkeljensen@h482.aau1x.klient.slv.site.aau.dk>
2022-10-20 08:51:15 +0200

    mutatoer class

---
+++ python-fuzzer/mutators/doc_pack_mutator.py
@@ -0,0 +1,18 @@
+from mutator import Mutator
+
+import random
+from typing import Any
+
+
+class DocPackMutator(Mutator):
+    def __init__(self):
+        # TODO: List mutator functions here
+        self.mutators = [self.change_nothing_mutator]
+
+    # TODO: Implement actual mutators - this is just placeholder code
+    def change_nothing_mutator(self, inp: Any):
+        return inp
+
+    def mutate(self, inp: Any) -> Any:
+        mutator = random.choice(self.mutators)
+        return mutator(inp)
--- python-fuzzer/mutators/mutator.py
+++ python-fuzzer/mutators/mutator.py
@@ -3,10 +3,6 @@ from typing import Any
 
 
 class Mutator(ABC):
-    @property
-    def mutators(self):
-        raise NotImplementedError
-
     @abstractmethod
     def mutate(self, inp: Any) -> Any:
         pass
