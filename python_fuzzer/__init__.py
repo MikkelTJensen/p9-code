a11600db6c9f57eacfa5b4078a8b243a48c04fec
Mikkel Jensen <mikkeljensen@h482.aau1x.klient.slv.site.aau.dk>
2022-10-20 13:45:15 +0200

    added __all__

--- python_fuzzer/__init__.py
+++ python_fuzzer/__init__.py
@@ -4,3 +4,10 @@ from mutators import *
 from parsers import *
 from runners import *
 from fuzzers import *
+
+__all__ = ["Client", "DocumentPackageClient",
+           "Logger", "SimpleLogger",
+           "Mutator", "DocumentPackageMutator",
+           "Parser", "DocumentPackageParser",
+           "Runner", "DocumentPackageRunner",
+           "Fuzzer", "DocumentPackageFuzzer"]
