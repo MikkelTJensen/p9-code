e28954cc672200d5c0c86d997c2440d23c7e0f58
Mikkel Jensen <mikkeljensen@h482.aau1x.klient.slv.site.aau.dk>
2022-10-20 11:10:44 +0200

    mutator clean up

--- python-fuzzer/mutators/doc_pack_mutator.py
+++ python-fuzzer/mutators/doc_pack_mutator.py
@@ -1,18 +1,18 @@
-from mutator import Mutator
-
 import random
-from typing import Any
+from typing import Any, List, Callable
+
+from mutator import Mutator
 
 
 class DocumentPackageMutator(Mutator):
     def __init__(self) -> None:
         # TODO: List mutator functions here
-        self.mutators = [self.change_nothing_mutator]
+        self.mutators: List[Callable[[Any], Any]] = [self.change_nothing_mutator]
 
     # TODO: Implement actual mutators - this is just placeholder code
     def change_nothing_mutator(self, inp: Any) -> Any:
         return inp
 
     def mutate(self, inp: Any) -> Any:
-        mutator = random.choice(self.mutators)
+        mutator: Callable[[Any], Any] = random.choice(self.mutators)
         return mutator(inp)
--- python-fuzzer/mutators/mutator.py
+++ python-fuzzer/mutators/mutator.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
 from typing import Any
+from abc import ABC, abstractmethod
 
 
 class Mutator(ABC):
