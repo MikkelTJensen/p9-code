d9460ba5e2f145b39f4573b838b4bbd93c0823ec
Mikkel Jensen <mikkeljensen@h428.aau1x.klient.slv.site.aau.dk>
2022-10-26 12:39:33 +0200

    lambda :)

--- python_fuzzer/runners/rasp_runner.py
+++ python_fuzzer/runners/rasp_runner.py
@@ -6,7 +6,7 @@ from .base_runner import BaseRunner
 class RaspRunner(BaseRunner):
     def __init__(self) -> None:
         # TODO: make a function which can send a scapy packet and replace it with "None" below
-        function: Callable[..., Any] = None
+        function: Callable[..., Any] = lambda x: x
         super().__init__(function)
 
     def run(self, func_inp: Any) -> Tuple[Any, str]:
