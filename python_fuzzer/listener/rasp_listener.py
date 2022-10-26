3d1827da97e541271c9ea556c25c53523ff02331
fulei <emil.fulei@gmail.com>
2022-10-26 11:17:09 +0200

    Rasp Listener

---
+++ python_fuzzer/listener/rasp_listener.py
@@ -0,0 +1,18 @@
+import socket
+from typing import Any
+from platform import system
+
+from .listener import Listener
+from python_fuzzer.loggers.logger  import Logger
+
+
+class RaspListener(Listener):
+    def __init__(self, logger: Logger, state_machine: Any) -> None:
+        self.LOGGER: logger
+        self.STATEMACHINE: state_machine
+
+    def run(self) -> None:
+        while(True):
+            
+        # TODO run in loop, log and change state
+            print("lol")
