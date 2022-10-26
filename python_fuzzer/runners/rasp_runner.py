ebf810b7764f55e6a13f3d26da13d13015c756b1
Mikkel Jensen <mikkeljensen@h428.aau1x.klient.slv.site.aau.dk>
2022-10-26 12:48:22 +0200

    variable name change

--- python_fuzzer/runners/rasp_runner.py
+++ python_fuzzer/runners/rasp_runner.py
@@ -4,12 +4,13 @@ from typing import Any, Tuple, Callable
 from .base_runner import BaseRunner
 from python_fuzzer.loggers.logger import Logger
 
+
 class RaspRunner(BaseRunner):
-    def __init__(self, logger: Logger) -> None:
+    def __init__(self, log: Logger) -> None:
         # TODO: make a function which can send a scapy packet and replace it with "None" below
         function: Callable[..., Any] = lambda x: x
         super().__init__(function)
-        self.logger = logger
+        self.logger = log
 
     def run(self, func_inp: Any) -> Tuple[Any, str]:
         pass
