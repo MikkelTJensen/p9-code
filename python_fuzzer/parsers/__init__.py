f9dc02618904bc634b353054bc62bd4888dc5e6c
Mikkel Jensen <mikkeljensen@h428.aau1x.klient.slv.site.aau.dk>
2022-10-26 14:03:52 +0200

    dont think there is more to do in the parser

--- python_fuzzer/parsers/__init__.py
+++ python_fuzzer/parsers/__init__.py
@@ -1,4 +1,4 @@
 from .input_parser import InputParser as Parser
-from .doc_pack_parser import DocumentPacketParser
+from .doc_pack_parser import PacketParser
 
-__all__ = ["Parser", "DocumentPacketParser"]
+__all__ = ["Parser", "PacketParser"]
