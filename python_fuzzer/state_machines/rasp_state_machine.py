f8df1b10f06b5698973108c9f0a71a09128cf1af
Mikkel Jensen <mikkeljensen@h428.aau1x.klient.slv.site.aau.dk>
2022-10-26 11:04:36 +0200

    state machines

---
+++ python_fuzzer/state_machines/rasp_state_machine.py
@@ -0,0 +1,15 @@
+from .state_machine import StateMachine
+
+
+class RaspStateMachine(StateMachine):
+    def __init__(self) -> None:
+        pass
+
+    def get_state(self) -> str:
+        pass
+
+    def set_state(self) -> str:
+        pass
+
+    def wait_for_change(self) -> str:
+        pass
