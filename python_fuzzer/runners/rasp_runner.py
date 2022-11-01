ae9cbc860930b2790f2f157782cdd3513b10585d
fulei <emil.fulei@gmail.com>
2022-11-01 09:09:24 +0100

    send packet method

--- python_fuzzer/runners/rasp_runner.py
+++ python_fuzzer/runners/rasp_runner.py
@@ -2,6 +2,7 @@ from typing import Any, Tuple, Callable
 from subprocess import run, PIPE
 from os import getcwd
 from os.path import join
+from scapy import sendrecv
 
 if __name__ == "__main__":
     from base_runner import BaseRunner
@@ -15,12 +16,16 @@ from python_fuzzer.loggers.logger import Logger
 class RaspRunner(BaseRunner):
     def __init__(self, log: Logger, path: str) -> None:
         # TODO: make a function which can send a scapy packet and replace it with "None" below
-        function: Callable[..., Any] = lambda x: x
+        function: Callable[..., Any] = lambda x:x
         super().__init__(function)
         self.logger = log
         self.path = path
 
     def run(self, func_inp: Any) -> Tuple[Any, str]:
+        pass
+
+    def send_packet(self, packet: Any) -> None:
+        sendrecv.sendp(packet)
         pass
 
     def start_process(self):
