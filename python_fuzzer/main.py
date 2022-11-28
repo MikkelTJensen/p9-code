1c72b96012a1aa3c5baa70a33dc09c78f863ef79
m-tolstrup <mikjen97@gmail.com>
2022-11-28 11:17:15 +0100

    argparse in main

--- python_fuzzer/main.py
+++ python_fuzzer/main.py
@@ -7,45 +7,56 @@ from runners import RaspRunner
 from mutators import PacketMutator
 from data_structures import Seed
 
-from os import getcwd
-from os.path import join
-import threading
+import os
+import argparse
 
 
-def run_listener(log: SimpleLogger, sm: RaspStateMachine) -> None:
-    listen: RaspListener = RaspListener(log, sm)
-    listen.run()
+def main(listen_for_traffic: bool) -> None:
+    # Get current working directory to create folders
+    cwd_path: str = os.getcwd()
+    if not cwd_path.endswith("python_fuzzer"):
+        cwd_path = os.path.join(cwd_path, "python_fuzzer")
 
+    # Initialize the logger
+    logger_path: str = os.path.join(cwd_path, "log_files")
+    log: SimpleLogger = SimpleLogger(logger_path)
 
-def run_fuzzer(cwd_path: str, log: SimpleLogger, sm: RaspStateMachine) -> None:
-    input_path: str = join(cwd_path, "packets")
-    process_path: str = join(cwd_path, "executables", "ClientExample")
-    parser: PacketParser = PacketParser(input_path)
-    seed: Seed = parser.load_seed()
+    # Initialize the state machine
+    sm: RaspStateMachine = RaspStateMachine()
 
-    mut: PacketMutator = PacketMutator()
+    # Initialize the runner
+    # TODO: ClientExample should probably be ServerExample
+    process_path: str = os.path.join(cwd_path, "executables", "ClientExample")
     run: RaspRunner = RaspRunner(log, process_path)
 
+    # Initialize and run the listener
+    if listen_for_traffic:
+        listen: RaspListener = RaspListener(log, sm, run)
+        listen.run()
+
+    # Parse the intercepted packets - or previously saved packets
+    packet_path: str = os.path.join(cwd_path, "packets")
+    parser: PacketParser = PacketParser(packet_path)
+    seed: Seed = parser.load_seed()
+
+    # Initialize the mutator
+    mut: PacketMutator = PacketMutator()
+
+    # Initialize and run the fuzzer
     fuzz: RaspFuzzer = RaspFuzzer(seed, mut)
     result = fuzz.multiple_runs(run, sm, len(seed))
     print(result)
 
 
-def main() -> None:
-    cwd_path: str = getcwd()
-    if not cwd_path.endswith("python_fuzzer"):
-        cwd_path = join(cwd_path, "python_fuzzer")
-
-    logger_path: str = join(cwd_path, "log_files")
-    log: SimpleLogger = SimpleLogger(logger_path)
-    sm: RaspStateMachine = RaspStateMachine()
-
-    listener_thread = threading.Thread(target=run_listener, args=(log, sm))
-    listener_thread.start()
-
-    fuzzer_thread = threading.Thread(target=run_fuzzer, args=(cwd_path, log, sm))
-    fuzzer_thread.start()
-
-
 if __name__ == '__main__':
-    main()
+    p = argparse.ArgumentParser(description="Arguments for network protocol fuzzing harness")
+
+    # Call main.py with "--l" flag to run the listener
+    p.add_argument("--l",
+                   default=False,
+                   action="store_true",
+                   help="Enable the listener - will execute before the fuzzer")
+
+    args = p.parse_args()
+
+    main(args.l)
