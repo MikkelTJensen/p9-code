46b03cd6f90cb88500c219a7695262dc1d5da82c
m-tolstrup <mikjen97@gmail.com>
2022-12-15 13:03:36 +0100

    clean up

--- python_fuzzer/mutators/packet_mutator.py
+++ python_fuzzer/mutators/packet_mutator.py
@@ -1,45 +1,50 @@
 import random
 from typing import Any, List, Callable
-from scapy.packet import *
+from scapy.packet import Packet
 
 from .mutator import Mutator
 
 
 class PacketMutator(Mutator):
-    def __init__(self) -> None:
+    def __init__(self, verbose: bool) -> None:
         # List mutator functions here
-        self.mutators: List[Callable[[Any], Any]] = [self.flip_bit_mutator, self.add_to_byte_mutator, self.remove_from_byte_mutator]
+        self.verbose: bool = verbose
+        self.mutators: List[Callable[[Any], Any]] = [self.flip_bit_mutator,
+                                                     self.add_to_byte_mutator,
+                                                     self.remove_from_byte_mutator]
 
-    def mutate(self, inp: Any) -> Any:
+    def mutate(self, packet: Packet) -> Packet:
+        data: bytes = packet[Raw].load
         mutator: Callable[[Any], Any] = random.choice(self.mutators)
-        return mutator(inp)
+        packet[Raw].load = mutator(data)
+        return packet
 
     #string methods of this
-    def flip_bit_mutator(self, inp: Any) -> Any:
-        pos = random.randint(0, len(inp[Raw].load) - 1)
-        c = chr(inp[Raw].load[pos])
-        bit = 1 << random.randint(0, 6)
-        new_c = chr(ord(c) ^ bit)
+    def flip_bit_mutator(self, data: bytes) -> bytes:
+        pos: int = random.randint(0, len(data) - 1)
+        c: chr = chr(data[pos])
+        bit: int = 1 << random.randint(0, 6)
+        new_c: chr = chr(ord(c) ^ bit)
 
-        inp[Raw].load = inp[Raw].load[:pos] + str.encode(new_c) + inp[Raw].load[pos + 1:]
+        data = data[:pos] + str.encode(new_c) + data[pos + 1:]
 
-        return inp
+        return data
 
-    def add_to_byte_mutator(self, inp: Any) -> Any:
-        pos = random.randint(0, len(inp[Raw].load) - 1)
-        c = inp[Raw].load[pos]
+    def add_to_byte_mutator(self, data: bytes) -> bytes:
+        pos: int = random.randint(0, len(data) - 1)
+        c: chr = chr(data[pos])
         c += random.randint(1, 36)
 
-        inp[Raw].load = inp[Raw].load[:pos] + str.encode(chr(c)) + inp[Raw].load[pos + 1:]
+        data = data[:pos] + str.encode(chr(c)) + data[pos + 1:]
 
-        return inp
+        return data
 
-    def remove_from_byte_mutator(self, inp: Any) -> Any:
-        pos = random.randint(0, len(inp[Raw].load) - 1)
-        c = inp[Raw].load[pos]
+    def remove_from_byte_mutator(self, data: bytes) -> bytes:
+        pos: int = random.randint(0, len(data) - 1)
+        c: chr = chr(data[pos])
         c -= random.randint(1, 36)
 
-        inp[Raw].load = inp[Raw].load[:pos] + str.encode(chr(c)) + inp[Raw].load[pos + 1:]
+        data = data[:pos] + str.encode(chr(c)) + data[pos + 1:]
 
-        return inp
+        return data
 
